4456d510555764e88ba94ca090d02f1b9c1eb543
erikvanlennep <erik.van.lennep@gmail.com>
2017-10-27 09:45:28 +0200

    Update userProfile with unanswered questions

---
+++ src/AppBundle/Controller/ProfileController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Form\InvitationType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use FOS\UserBundle\Controller\ProfileController as BaseController;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Invitation;
+use VMelnik\DoctrineEncryptBundle\Configuration\Encrypted;
+
+
+/**
+ * @Route("/profile")
+ * @Security("has_role('ROLE_USER')")
+ */
+class ProfileController extends BaseController
+{
+    /**
+     * @Route("/")
+     * @Template()
+     *
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $questions = $em->getRepository('AppBundle:Question')->findAllUnanswered();
+
+        $user = $this->getUser();
+//        if (!is_object($user) || !$user instanceof UserInterface) {
+//            throw new AccessDeniedException('This user does not have access to this section.');
+//        }
+
+        return $this->render('profile/index.html.twig', array(
+            'user' => $user,
+            'questions' => $questions,
+        ));
+
+
+
+
+//        if(!$questions) { $questions = "test";}
+//        $paginator = $this->get('knp_paginator');
+//        $pagination = $paginator->paginate(
+//            $questions,
+//            $request->query->getInt('page', 1)/*page number*/,
+//            5/*limit per page*/
+//        );
+    }
+}
